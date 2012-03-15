f4ff2004cbdf6506a77421545804193c384eb151
Phil Monroe <phil@philmonroe.com>
2012-03-14 22:33:51 -0700

    prob3 part 2

--- problem3.m
+++ problem3.m
@@ -34,7 +34,6 @@ f = ReadSignal('msignal');
 N = length(f);
 x = 1:N;
 
-
 % Plot Signal -------------------------------------------------------------
 subplot(3, 1, 1), plot(x, f);
 axis tight;
@@ -45,5 +44,7 @@ subplot(3, 1, 2), WindowFT(f,50,1,'Gaussian');
 title('Short Time Fourier Transform', 'FontSize', 14),xlabel('Time', 'FontSize', 14), ylabel('Frequency', 'FontSize', 14)
 
 % Plot CWT ----------------------------------------------------------------
-subplot(3, 1, 3), cwt(f,1:32,'sym8','plot'); 
-title('Symlet CWT - sym8', 'FontSize', 14), xlabel('Time', 'FontSize', 14), ylabel('Scale', 'FontSize', 14)
+rwt = RWT(f, 8, 'Sombrero', 1, 4);
+rwt = fliplr(rwt);
+subplot(3, 1, 3), ImageRWT(rwt,'Individual','gray','log'); 
+title('Continuous Wavelet Transform', 'FontSize', 14), xlabel('Time', 'FontSize', 14), ylabel('Scale', 'FontSize', 14)
